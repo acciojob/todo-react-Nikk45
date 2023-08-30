ccd2198e3911eea0ea54210aef57c72a067aba23
Nikk45 <nicksin36@gmail.com>
2023-08-30 19:48:07 +0530

    submit 1

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,42 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const [taskName, setTaskName] = useState('')
+  const [taskList,setTaskList] = useState([])
+
+  function addTask(){
+    setTaskList([...taskList, {id:crypto.randomUUID(), name:taskName}])
+    setTaskName('')
+  }
+
+  function deleteTask(id){
+      setTaskList(taskList.filter((task)=>
+        task.id!==id
+      ))
+  }
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <h3>To-Do List</h3>
+        <div className="inputfield">
+          <input type="text" value={taskName} onChange={(e)=>setTaskName(e.target.value)}/>
+          <button onClick={addTask}>Add Todo</button>
+        </div>
+        
+      <div>
+        {
+          taskList.map(task=>
+            <div className="listItems" key={task.id}>
+              <p>{task.name}</p><button onClick={()=>deleteTask(task.id)}>Delete</button>
+            </div>)
+        }
+      </div>
+
+
     </div>
   )
 }
